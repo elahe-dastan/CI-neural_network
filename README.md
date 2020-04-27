4a30427464cd9fad4279e72098831c4c90193805
raha <elahe.dstn@gmail.com>
2020-04-27 21:47:35 +0430

    Added comment to code

---
+++ README.md
@@ -0,0 +1,9 @@
+The aim of this homework is to know how perceptrons and neural networks work better, I'm going to report what I did <br/>
+step by step <br/>
+
+#### 1. Scattering data
+First of all I want to scatter the data and show each group with a different color. <br/>
+This is what I got<br/>
+![](Figures/Figure_1.png)
+red color is used to show label 1 and blue is used to show 0<br/>
+
--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import numpy as np
 from neural_network import NeuralNetwork
 
 
+# This function gets the name of a CSV file and returns the coordinates of the points
+# which are the inputs of the neural network and the labels which are the outputs
 def read_data(file_name):
     X = []
 
@@ -13,14 +15,21 @@ def read_data(file_name):
     X.append(data["X1"])
     X.append(data["X2"])
 
-    return np.array(np.transpose(X)), data["Label"]
+    # Now array X has two rows, first row contains all the numbers for X1 and the
+    # second row contains all the numbers for X2 but I want an output in which
+    # the number of rows is equal to the number of data and each row contains
+    # one X1 and X2 so I got the transpose of array X
+    Xt = np.transpose(X)
+
+    return np.array(Xt), data["Label"]
 
 
 def plot(X, y):
-    plt.title('Scatter plot')
     plt.xlabel('x1')
     plt.ylabel('x2')
 
+    # If the label is predicted as 1 it will be shown by red color and if this is
+    # predicted as 0 it's color will be blue
     for xi, label in zip(X, y):
         if label > 0.5:
             plt.scatter(xi[0], xi[1], c="red")
